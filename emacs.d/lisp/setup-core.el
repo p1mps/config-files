e1137484134a022a22ee42ff040146bbca0eafe8
Andrea Imparato <imparato.andrea@gmail.com>
2018-06-11 10:54:30 +0200

    Set emacs backup directory

--- emacs.d/lisp/setup-core.el
+++ emacs.d/lisp/setup-core.el
@@ -48,6 +48,23 @@
             (eyebrowse-mode t)
             (setq eyebrowse-new-workspace t)))
 
+;; make backup to a designated dir, mirroring the full path
+
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let* (
+        (backupRootDir "~/.emacs.d/emacs-backup/")
+        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
+        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+        )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+  )
+)
+
+(setq make-backup-file-name-function 'my-backup-file-name)
+
 (global-linum-mode 1)
 
 ;; no tabs and 4 spaces
