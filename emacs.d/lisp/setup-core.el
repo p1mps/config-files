e8a1696896e1b03ca2366dddcb68099ccee82688
Andrea Imparato <imparato.andrea@gmail.com>
2018-06-13 23:56:43 +0200

    Set backup directory and disable lock files

--- emacs.d/lisp/setup-core.el
+++ emacs.d/lisp/setup-core.el
@@ -48,22 +48,12 @@
             (eyebrowse-mode t)
             (setq eyebrowse-new-workspace t)))
 
-;; make backup to a designated dir, mirroring the full path
+;; put all backup files into ~/emacs_backups
+(setq backup-by-copying t)
+(setq backup-directory-alist '(("." . "~/emacs_backups")))
 
-(defun my-backup-file-name (fpath)
-  "Return a new file path of a given file path.
-If the new path's directories does not exist, create them."
-  (let* (
-        (backupRootDir "~/.emacs.d/emacs-backup/")
-        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
-        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
-        )
-    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
-    backupFilePath
-  )
-)
-
-(setq make-backup-file-name-function 'my-backup-file-name)
+;; disable lock files
+(setq create-lockfiles nil)
 
 (global-linum-mode 1)
 
