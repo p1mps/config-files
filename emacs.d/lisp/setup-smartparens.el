63d45ccce0e41a27a89253cf61454064dbf959fe
Andrea Imparato <imparato.andrea@gmail.com>
2018-06-13 23:42:52 +0200

    Add smartparens

---
+++ emacs.d/lisp/setup-smartparens.el
@@ -0,0 +1,10 @@
+(use-package smartparens-config
+    :ensure smartparens
+    :config
+    (progn
+      (show-smartparens-global-mode t)))
+
+(add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
+(add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
+
+(provide 'setup-smartparens)
