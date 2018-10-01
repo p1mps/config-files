d18b558fe584d0c01ba69ca9814a97a1609405a1
Andrea Imparato <imparato.andrea@gmail.com>
2018-10-01 22:29:15 +0200

    Add rainbow-delimiters

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,10 +23,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
+    ("b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" default)))
  '(package-selected-packages
    (quote
-    (smart-mode smart-mode-line-powerline-theme powerline evil-cleverparens evil-smartparens pdf-tools markdown-mode cider exec-path-from-shell smartparens move-text ag eyebrowse gherkin-mode perspeen exwm dashboard fill-column-indicator company company-mode helm-projectile helm linum-mode dump-jump dumb-jump magit php-mode flycheck linum-relative use-package zenburn-theme evil-visual-mark-mode))))
+    (rainbow-delimiters rainbow-delimiters-mode smart-mode smart-mode-line-powerline-theme powerline evil-cleverparens evil-smartparens pdf-tools markdown-mode cider exec-path-from-shell smartparens move-text ag eyebrowse gherkin-mode perspeen exwm dashboard fill-column-indicator company company-mode helm-projectile helm linum-mode dump-jump dumb-jump magit php-mode flycheck linum-relative use-package zenburn-theme evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/lisp/setup-core.el
+++ emacs.d/lisp/setup-core.el
@@ -62,6 +62,10 @@
   :ensure t
   :config (move-text-default-bindings))
 
+(use-package rainbow-delimiters
+  :ensure t
+  :config (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
 (use-package eyebrowse
   :ensure t
   :diminish eyebrowse-mode
