722f11628efbb209aeeedf83e4690ad5e7a266b4
Andrea Imparato <imparato.andrea@gmail.com>
2019-05-24 00:00:43 +0200

    add eastwood proto-repl cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,16 +1,8 @@
-{:user  {
-         ;; :plugins [[refactor-nrepl "2.4.0"]
-         ;;           [cider/cider-nrepl "0.18.0"]]
-
-         ;; :plugins [[cider/cider-nrepl "0.19.0-SNAPSHOT"]
-         ;;           [refactor-nrepl "2.4.0"]
-         ;;           [lein-ancient "0.6.15"]
-         ;;           [jonase/eastwood "0.3.3"]
-         ;;           [lein-pprint "1.2.0"]
-         ;;           [lein-try "0.4.3"]
-         ;;           [lein-cloverage "1.0.13"]]
-         ;; :middleware [cider-nrepl.plugin/middleware
-         ;;              refactor-nrepl.plugin/middleware]
-         ;; :dependencies [[nrepl "0.5.3"]
-         ;;                [org.clojure/tools.namespace "0.2.11"]]
-         }}
+{:user
+  {
+    :plugins [
+        [cider/cider-nrepl "0.21.1"]
+        [jonase/eastwood "0.3.5"]
+        [proto-repl "0.3.1"]]
+  }
+ }
